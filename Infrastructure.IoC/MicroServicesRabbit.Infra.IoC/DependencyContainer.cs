3af2a45e4ffe72e163cdeb6ea6a45cffd0105738
blagojcej <blagojcejovanoski@yahoo.com>
2019-12-06 21:55:40 +0100

    Completed MicroServicesRabbit.Infrastructure.IoC project.

---
+++ Infrastructure.IoC/MicroServicesRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MicroservicesRabbit.Domain.Core.Bus;
+using MicroservicesRabbit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroServicesRabbit.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domain bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
